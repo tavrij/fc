53a5559b291774ccf3e20997b2a32126b5cbfa34
tavrij <tavrij@gmail.com>
2018-04-25 19:17:49 +0430

    okdd

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php echo "Working ...";
-	?>
+<!doctype html>
+<html>
+<head>
+    <title>Working OK ...</title>
+
+</head>
+<body>
+    <div>
+		<h1>$ - Working OK ...</h1>
+	</div>
+</body>
+</html>
